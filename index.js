23d8268a84ba148acb776da045c0415c5f1db292
Usman Masood <usmanm@fastmail.fm>
2021-04-21 09:15:34 -0700

    ...

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const github = require('@actions/github');
 
 const main = async () => {
   const token = core.getInput('github-token');
   const approveLabel = core.getInput('approve-label');
 
-  const pullRequest = context.payload.pull_request;
+  const pullRequest = github.context.payload.pull_request;
   if (!pullRequest) {
-    core.info("Could not get pull request from context, exiting");
+    console.log("Could not get pull request from context, exiting");
     return;
   }
 
-  core.info(pullRequest);
+  console.log(pullRequest);
 
   const labels = pullRequest.labels.map(label => label.name);
   if (!labels.includes(approveLabel)) {
-    core.info("PR does not include approve-label, exiting");
+    console.log("PR does not include approve-label, exiting");
     return;
   }
 
-  core.info(labels);
+  console.log(labels);
 
-  const octokit = new GitHub(token);
+  const octokit = github.getOctokit(token);
 
   await octokit.pulls.createReview({
     ...context.repo,
